f48ae90e410ef83a4ced6d6c0e8661789462aedc
Intimidating-Name <njelbaum2@gmail.com>
2018-10-06 12:00:23 -0400

    proof of concept

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
+import face_recognition
 import pyttsx3
-#import face_recognition
-import cv2
-video_capture = cv2.VideoCapture(0)
 engine = pyttsx3.init()
-engine.say('Sally sells seashells by the seashore.')
-engine.say('The quick brown fox jumps over the lazy dog.')
-engine.runAndWait()
+
+known_people = {
+    "known/tomhanks1.jpeg":"tom hanks",
+    "known/fredrogersstamp1.png":"fred rogers",
+    "you.jpeg":"me.self",
+    "a":"jim"
+}
+
+known_image = face_recognition.load_image_file("known/tomhanks1.jpeg")
+unknown_image = face_recognition.load_image_file("known/fredrogersstamp1.png")
+tom_encoding = face_recognition.face_encodings(known_image)[0]
+unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+results = face_recognition.compare_faces([tom_encoding], unknown_encoding)
+
+if results[0] is True:
+    engine.say("Hello")
+    print("Hello " + known_people[known_image])
+else:
+    engine.say("Goodbyeasdfyuhkjasdrfrgur")
+    print("Goodbye")
+
+#engine.runAndWait()
+
+
+
+
